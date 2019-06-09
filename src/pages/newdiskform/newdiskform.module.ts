7a1fd5eddf8b986510480fbe0dd6bf63785ffacf
juanrdzbaeza <juanrdzbaeza@gmail.com>
2019-06-09 11:50:33 +0200

    006. Añadida vista-controlador para el formulario de creación de un disco

---
+++ src/pages/newdiskform/newdiskform.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { NewdiskformPage } from './newdiskform';
+
+@NgModule({
+  declarations: [
+    NewdiskformPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(NewdiskformPage),
+  ],
+})
+export class NewdiskformPageModule {}
