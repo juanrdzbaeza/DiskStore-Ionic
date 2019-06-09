2b8b3b715fc8b3cbc356951020ceaf5a4c3e8d16
juanrdzbaeza <juanrdzbaeza@gmail.com>
2019-06-09 11:52:13 +0200

    007. Modifico estructura para la lista de discos, ahora se controla con un botón por disco que resulta en un contenedor completo que contiene la imagen el titulo y la descripción del disco, este botón implementa la funcionalidad para pasar a la vista del disco.

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -3,6 +3,8 @@ import { DiskstoreProvider } from './../../providers/diskstore/diskstore';
 
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DiskPage } from '../disk/disk';
+import { NewdiskformPage } from '../newdiskform/newdiskform';
 
 @Component({
   selector: 'page-list',
@@ -11,21 +13,37 @@ import { NavController } from 'ionic-angular';
 export class ListPage {
   disks = [];
   disk = [];
-
+  A = [];
+  B = [];
+  deletedDisk ;
 
   constructor(
     public navCtrl: NavController, 
     private diskstoreProvider: DiskstoreProvider) {
     diskstoreProvider.getDisks().subscribe((result: any) => {
       this.disks = result;
-      console.log(result);
+      //console.log(result);
     })
   }
 
-  getDisk(id: any) {
-    this.diskstoreProvider.getDisk(id).subscribe((result: any) =>{
+  getDisk(_id: any) {
+    this.diskstoreProvider.getDisk(_id).subscribe((result: any) =>{
       this.disk = result;
-      console.log(result);
+      this.A = result.songs.A;
+      this.B = result.songs.B;
+      //console.log(result.songs.A);
+      //console.log(result.songs.B);
+      this.navCtrl.push(DiskPage, {
+        "disk": this.disk, 
+        "A": this.A, 
+        "B": this.B
+      });
     })
+  } // fin del getDisk
+
+  addDisk() {
+    this.navCtrl.push(NewdiskformPage,{});
   }
+
+  
 }
