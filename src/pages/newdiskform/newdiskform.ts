506f8824bb91e90c1aedfe3a9a15017025aa194c
juanrdzbaeza <juanrdzbaeza@gmail.com>
2019-06-09 17:37:53 +0200

    011. Termino de definir la plantilla del formulario, e implemento la funcionalidad al controlador del formulario.

--- src/pages/newdiskform/newdiskform.ts
+++ src/pages/newdiskform/newdiskform.ts
@@ -1,6 +1,9 @@
+import { DiskstoreProvider } from './../../providers/diskstore/diskstore';
+
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ListPage } from '../list/list';
 
 /**
  * Generated class for the NewdiskformPage page.
@@ -17,27 +20,49 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class NewdiskformPage {
 
   diskForm: FormGroup;
+  disk = [];
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public formBuilder: FormBuilder) {
+    public formBuilder: FormBuilder,
+    private diskstoreProvider: DiskstoreProvider) {
       this.diskForm = this.createDiskForm();
   }
 
   saveData() {
-    console.log("fjaskdjflsakdjflkasdfj");
+    this.diskstoreProvider.saveDisk(this.diskForm.value).subscribe((result: any) => {
+      this.disk = result;
+      //console.log(this.disk);
+      this.navCtrl.push(ListPage,{})
+    })
   }
 
   private createDiskForm() {
     return this.formBuilder.group({
-      title: ['', Validators.required],
+      title: [''],
+      genre: [''],
+      artist: [''],
+      publisher: [''],
+      song1:[''],
+      song2:[''],
+      song3:[''],
+      song4:[''],
+      song5:[''],
+      song6:[''],
+      song7:[''],
+      song8:[''],
+      song9:[''],
+      song10:[''],
+      song11:[''],
+      song12:[''],
+      description:[''],
+      image_url:['']
     });
   }
 
-
   ionViewDidLoad() {
-    console.log('ionViewDidLoad NewdiskformPage');
+    //console.log('ionViewDidLoad NewdiskformPage');
   }
 
 }
